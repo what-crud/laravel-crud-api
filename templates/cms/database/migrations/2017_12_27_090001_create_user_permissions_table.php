957620433f64f8710eaa0b1cefbc313824d0b1fd
Szczepan Masny <szczepanmasny@gmail.com>
2019-05-02 16:50:35 +0200

    cms init

---
+++ templates/cms/database/migrations/2017_12_27_090001_create_user_permissions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('permission_id')->unsigned();
+            $table->foreign('permission_id')->references('id')->on('permissions');
+            $table->unique(['user_id', 'permission_id']);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_permissions');
+    }
+}
---
+++ templates/cms/database/seeds/InitSeeder.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin\User;
+
+class InitSeeder extends Seeder
+{
+    public function run()
+    {
+
+        DB::table('permissions')
+        ->insert([
+            [
+                'name' => 'Blog',
+                'code' => 'BLOG',
+            ],
+            [
+                'name' => 'Store',
+                'code' => 'STORE',
+            ],
+            [
+                'name' => 'Administration',
+                'code' => 'ADMIN',
+            ]
+        ]);
+
+        DB::table('user_types')
+        ->insert([
+            [
+                'name' => 'Guest',
+                'read' => true,
+                'insert' => false,
+                'update' => false,
+                'delete' => false,
+                'admin' => false,
+                'active' => true,
+            ],
+            [
+                'name' => 'User',
+                'read' => true,
+                'insert' => true,
+                'update' => true,
+                'delete' => true,
+                'admin' => false,
+                'active' => true,
+            ],
+            [
+                'name' => 'Admin',
+                'read' => true,
+                'insert' => true,
+                'update' => true,
+                'delete' => true,
+                'admin' => true,
+                'active' => true,
+            ]
+        ]);
+
+        DB::table('users')
+        ->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('1234'),
+                'user_type_id' => 3
+            ]
+        ]);
+
+        DB::table('user_permissions')
+        ->insert([
+            ['user_id' => 1,'permission_id' => 1],
+            ['user_id' => 1,'permission_id' => 2],
+            ['user_id' => 1,'permission_id' => 3]
+        ]);
+
+        DB::table('languages')
+        ->insert([
+            ['name' => 'Polski','priority' => 1],
+            ['name' => 'Angielski','priority' => 2],
+        ]);
+    }
+}
