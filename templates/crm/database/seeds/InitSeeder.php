53ba9e72707968ec823fa9c075cec41d23a8a1d7
Szczepan Masny <szczepanmasny@gmail.com>
2019-10-07 23:40:07 +0200

    update crm init seeder

--- templates/crm/database/seeds/InitSeeder.php
+++ templates/crm/database/seeds/InitSeeder.php
@@ -7,21 +7,59 @@ class InitSeeder extends Seeder
 {
     public function run()
     {
-        $user = new User();
-        $user->name = 'admin';
-        $user->email = 'admin@admin.com';
-        $user->password = bcrypt('1234');
-        $user->save();
+
+        DB::table('user_types')
+        ->insert([
+            [
+                'name' => 'Guest',
+                'read' => true,
+                'insert' => false,
+                'update' => false,
+                'delete' => false,
+                'admin' => false,
+                'active' => true,
+            ],
+            [
+                'name' => 'User',
+                'read' => true,
+                'insert' => true,
+                'update' => true,
+                'delete' => true,
+                'admin' => false,
+                'active' => true,
+            ],
+            [
+                'name' => 'Admin',
+                'read' => true,
+                'insert' => true,
+                'update' => true,
+                'delete' => true,
+                'admin' => true,
+                'active' => true,
+            ]
+        ]);
+
+        DB::table('users')
+        ->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('1234'),
+                'user_type_id' => 3
+            ]
+        ]);
 
         DB::table('permissions')
         ->insert([
             [
                 'name' => 'CRM',
                 'code' => 'CRM',
+                'path' => 'crm'
             ],
             [
                 'name' => 'administracja',
                 'code' => 'ADMIN',
+                'path' => 'admin'
             ]
         ]);
 
