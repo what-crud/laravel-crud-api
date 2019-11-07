ff7f8b818354dbc89be4baa693800f9f8bf7c697
Szczepan Masny <szczepanmasny@gmail.com>
2019-11-07 12:46:28 +0100

    add menu items table migration to cms example

---
+++ templates/cms/database/migrations/2019_11_07_112300_create_menu_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+            $table->string('path');
+            $table->integer('parent_menu_item_id')->unsigned()->nullable();
+            $table->foreign('parent_menu_item_id')->references('id')->on('menu_items');
+            $table->integer('order')->unsigned()->nullable();
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
